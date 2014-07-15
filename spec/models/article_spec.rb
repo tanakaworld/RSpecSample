674e593af0b57c48d8aa5755a5c4bd541fd3327e
yutaro <y.tanaka.kcbbf@gmail.com>
2014-07-16 02:21:59 +0900

    Success my first spec.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article do
+  context "[Set Title and Body]" do
+    before do
+      @article = Article.new(
+          title: "My First Blog",
+          body: "This is my first article."
+      )
+    end
+    it "Set Title" do
+      expect(@article.title).to eq "My First Blog"
+    end
+    it "Set Body" do
+      expect(@article.body).to eq "This is my first article."
+    end
+  end
 end
