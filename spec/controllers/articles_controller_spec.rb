661aa86be1390ec9e3253b0a48fd2f0daf3fe65a
yutaro <y.tanaka.kcbbf@gmail.com>
2014-07-20 02:29:20 +0900

    Add Controller tests.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,9 +3,24 @@ require 'spec_helper'
 describe ArticlesController, :type => :controller do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    before do
+      @article1 = Aritcle.create(
+          title: "Article 1",
+          body: "Hello"
+      )
+      @article2 = Article.create(
+          title: "Article 2",
+          body: "World"
+      )
       get 'index'
+    end
+    it "returns http success" do
       expect(response).to be_success
+    end
+    it "returns All articles" do
+      expect(assigns[:articles]).to match_array(
+                                        [@article1, @article2]
+                                    )
     end
   end
 
