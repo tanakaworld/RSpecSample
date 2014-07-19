b0a17dcc03d0c292b9ce395c8506a5402917f8c1
yutaro <y.tanaka.kcbbf@gmail.com>
2014-07-20 02:39:04 +0900

    Add View Test.

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe "articles/index.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    assign :articles, [
+        Article.create(
+            title: "My first blog",
+            body: "Hello"
+        )
+    ]
+    render
+  end
+
+  it "Render article title" do
+    expect(rendered).to match /My first blog/
+  end
+
+  it "Render article body" do
+    expect(rendered).to match /Hello/
+  end
+
 end
